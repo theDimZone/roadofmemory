94c78f716d5101692eb37237fb857bf6c1834e4f
«QueTuePlay» <aleks.berkovets@yandex.ru>
2020-04-04 21:52:05 +0300

    SomeBack

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,26 @@
 class UserController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(veteran_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private def veteran_params
+    params.require(:veteran).permit(:name)
   end
 end
