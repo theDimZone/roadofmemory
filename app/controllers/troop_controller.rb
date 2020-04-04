454e4155eedb09c0db5576bdb206311f8addb499
Dmitry Kabarukhin <thedimzone99@gmail.com>
2020-04-05 00:20:16 +0300

    temp

--- app/controllers/troop_controller.rb
+++ app/controllers/troop_controller.rb
@@ -1,11 +1,14 @@
 class TroopController < ApplicationController
   def show
+    @troop = Troop.find(params[:id])
   end
 
   def index
+    @troops = Troop.where(parent_id: nil)
   end
 
   def new
+
   end
 
   def create
