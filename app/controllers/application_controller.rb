ec5c1b8ccf652a95a748b13573dd49b6cb5bea1f
Dmitry Kabarukhin <thedimzone99@gmail.com>
2020-04-05 02:15:15 +0300

    add some auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  require 'digest/md5'
+  def is_cookie_valid_vk
+    #конфиг пока здесь..
+    app_id = "7391635"
+    user_id = cookies[:socialid]
+    secret_key = "GGmuNgPTjbRAevzPok45"
+
+    md5 = Digest::MD5.hexdigest(app_id + user_id + secret_key)
+
+    return md5 == cookies[:hash]
+  end
+
+  def is_authored
+    return cookies[:id].nil?
+  end
+
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,20 @@
 class LoginController < ApplicationController
   def index
+    if is_authored redirect_to :controller => 'user', :action => 'show', :id => cookies[:id]
   end
 
   def get_token
+    hash = params[:hash]
+    userid = params[:uid]
+    name = params[:first_name] + " " + params[:last_name]
+    user = User.where(socialid: userid, socialtype: "vk").first_or_create(name: name, score: 0)
+
+    cookies.permanent[:hash] = hash
+    cookies.permanent[:id] = user.id
+    cookies.permanent[:socialid] = userid
+    cookies.permanent[:socialtype] = "vk"
+    cookies.permanent[:name] = name
+
+    redirect_to :controller => 'user', :action => 'show', :id => user.id
   end
 end
