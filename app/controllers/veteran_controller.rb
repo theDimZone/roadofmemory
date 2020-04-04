2218d39d296c0cdbc320ab1e800e30eea0337b62
Dmitry Kabarukhin <thedimzone99@gmail.com>
2020-04-04 16:24:46 +0300

    add veteran

--- app/controllers/veteran_controller.rb
+++ app/controllers/veteran_controller.rb
@@ -3,4 +3,8 @@ class VeteranController < ApplicationController
       @veteran = Veteran.find(params[:id])
 
   end
+
+  def add
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/', to: 'index#index'
+  get '/veteran/add', to: 'veteran#add'
   get '/veteran/:id', to: 'veteran#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
