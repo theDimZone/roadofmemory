956e617cde6114353ef2ed385c44ecbd2281e091
farhad <farhadnikolaev93@gmail.com>
2020-04-05 05:28:10 +0300

    AUodn

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -19,4 +19,19 @@ class LoginController < ApplicationController
 
     redirect_to :controller => 'user', :action => 'show', :id => user.id
   end
+
+  def get_token_Od
+    hash = params[:hash]
+    userid = params[:uid]
+    name = params[:first_name] + " " + params[:last_name]
+    user = User.where(socialid: userid, socialtype: "od").first_or_create(name: name, score: 0)
+
+    cookies.permanent[:hash] = hash
+    cookies.permanent[:id] = user.id
+    cookies.permanent[:socialid] = userid
+    cookies.permanent[:socialtype] = "od"
+    cookies.permanent[:name] = name
+
+    redirect_to :controller => 'user', :action => 'show', :id => user.id
+  end
 end
