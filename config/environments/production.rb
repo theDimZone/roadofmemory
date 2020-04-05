d621e7bf5bf36c43a5521c5a8488d44b6655f273
Dmitry Kabarukhin <thedimzone99@gmail.com>
2020-04-05 12:04:58 +0300

    tt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,12 +4,15 @@ Rails.application.configure do
     storage: :s3,
     s3_host_name:"#{ENV.fetch('S3_BUCKET_NAME')}.s3.eu-central-1.amazonaws.com",
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      bucket: "",
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     }
   }
+
+  #  s3_host_name: ENV.fetch('AWS_ENDPOINT'),
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
