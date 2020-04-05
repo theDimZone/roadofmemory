eff534933020c384603ef070dbc3fb80ba406aa0
Dmitry Kabarukhin <thedimzone99@gmail.com>
2020-04-05 07:50:46 +0300

    new

--- app/controllers/troop_controller.rb
+++ app/controllers/troop_controller.rb
@@ -1,6 +1,12 @@
 class TroopController < ApplicationController
   def show
     @troop = Troop.find(params[:id])
+
+    ids = []
+    ids << @troop.descendants.ids
+    ids << @troop.id
+
+    @allreferencetroops = Troop.find(ids)
   end
 
   def index
