a2bdc39d8f5a93029fda7677f303caa7cd88eed6
Dmitry Kabarukhin <thedimzone99@gmail.com>
2020-04-05 12:17:51 +0300

    fix

--- app/models/veteran.rb
+++ app/models/veteran.rb
@@ -8,7 +8,7 @@ class Veteran < ApplicationRecord
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }
+  }, :url  => ":s3_eu_url"
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
